9962857997d3a1b604266a61a8c3371517cc6e23
MatthewBloemke <mattbloemke@gmail.com>
2023-08-01 15:10:29 +0000

    added new bio info

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,48 +1,79 @@
 import Image from 'next/image';
 import HomeImage from '../../../public/HomeImage.jpeg';
+import truck from '../../../public/truck.png';
 
 const Page = () => {
   return (
-    <div className="bg-[#f5f5f5] mt-1">
-      <h1 className="text-center py-1">Brent Lester</h1>
-      <div className="flex justify-center flex-col md:flex-row items-center ">
-        <Image
-          src={HomeImage}
-          alt="homeimage"
-          height={500}
-          className="md:ml-[6%]"
-        />
-        <div className="mb-24 mt-2 mx-[6%]">
-          <h4>
-            Working in HVAC services since 1987, Brent Lester founded Lester
-            Heat and Air in 2016 to provide heating and air conditioning
-            services to Kiowa county and the surrounding areas.
-          </h4>
-          <br />
-          <h4>
-            With extensive knowledge on unit repair and maintenance (as well the
-            HVAC industry at large), Brent also works as an Industrial
-            Coordinator/Trainer for the Business Industries Service Department
-            at Caddo Kiowa Technology Center. You can trust Brent to provide
-            knowledge and experience when working on your home’s heating and air
-            conditioning needs!
-          </h4>
-          <br />
-          <h2>Education & Credentials:</h2>
-          <h4 className="font-semibold">
-            Bachelor of Science in Business Management
-          </h4>
-          <h4>University of Phoenix, 2002</h4>
-          <br />
-          <h4 className="font-semibold">
-            Associate Degree in Applied Science HVACR
-          </h4>
-          <h4>Oklahoma State University Tech. Okmulgee, 1987</h4>
-          <br />
-          <h4>
-            Standard Teaching Certificate for TRADE/INDUSTRIAL EDUCATION
-            Oklahoma State Dept. of Ed.
-          </h4>
+    <div>
+      <h1 className='text-center bg-[#f5f5f5] mt-1'>
+        Lester <span className="text-[#ff3f3f]">Heat</span> and{' '}
+        <span className="text-[#2ea4fb]">Air</span>
+      </h1>
+      <div className="bg-[#f5f5f5] mt-1">
+        <div className="flex justify-center flex-col md:flex-row items-center ">
+          <div className="mb-24 mt-2 mx-[6%]">
+            <h3>
+              At Lester Heat and Air, we pride ourselves on customer satisfaction through providing knowledge and over 35 years of experience to each HVAC job we work on. 
+            </h3>
+            <br/>
+            <h3>
+              Servicing all brands of HVAC equipment, whether residential or light commercial projects, Lester Heat and Air will get the job done. 
+            </h3>
+            <br/>
+            <h3>
+              We also sell Trane and Lennox products for new installations. Ask us for a quote and inquire about our financing options to get your heat and air conditioning units operating quickly!
+            </h3>
+          </div>
+          <Image
+            src={truck}
+            alt="homeimage"
+            height={500}
+            className="md:ml-[6%]"
+          />
+        </div>
+      </div>
+    
+      <h1 className="text-center bg-[#f5f5f5] mt-20">Brent Lester</h1>
+      <div className="bg-[#f5f5f5] mt-1">
+        <div className="flex justify-center flex-col md:flex-row items-center ">
+          <Image
+            src={HomeImage}
+            alt="homeimage"
+            height={500}
+            className="md:ml-[6%]"
+          />
+          <div className="mb-24 mt-2 mx-[6%]">
+            <h4>
+              Working in HVAC services since 1987, Brent Lester founded Lester
+              Heat and Air in 2016 to provide heating and air conditioning
+              services to Kiowa county and the surrounding areas.
+            </h4>
+            <br />
+            <h4>
+              With extensive knowledge on unit repair and maintenance (as well the
+              HVAC industry at large), Brent also works as an Industrial
+              Coordinator/Trainer for the Business Industries Service Department
+              at Caddo Kiowa Technology Center. You can trust Brent to provide
+              knowledge and experience when working on your home’s heating and air
+              conditioning needs!
+            </h4>
+            <br />
+            <h2>Education & Credentials:</h2>
+            <h4 className="font-semibold">
+              Bachelor of Science in Business Management
+            </h4>
+            <h4>University of Phoenix, 2002</h4>
+            <br />
+            <h4 className="font-semibold">
+              Associate Degree in Applied Science HVACR
+            </h4>
+            <h4>Oklahoma State University Tech. Okmulgee, 1987</h4>
+            <br />
+            <h4>
+              Standard Teaching Certificate for TRADE/INDUSTRIAL EDUCATION
+              Oklahoma State Dept. of Ed.
+            </h4>
+          </div>
         </div>
       </div>
     </div>
